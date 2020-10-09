3508b432043f25962aa5a83ea42cdca34cb190ed
Georgios Giatsidis <georgegiats@trigonongroup.com>
2020-10-09 18:04:53 +0300

    Implement linked list

---
+++ src/LinkedList.js
@@ -0,0 +1,69 @@
+const Node = require('./Node');
+
+class LinkedList {
+    constructor() {
+        this.left = null;
+        this.right = null;
+    }
+
+    pushLeft(value) {
+        const newNode = new Node(value, this.left, null);
+
+        if (this.left) {
+            this.left.previous = newNode;
+        } else {
+            this.right = newNode;
+        }
+
+        this.left = newNode;
+    }
+
+    pushRight(value) {
+        const newNode = new Node(value, null, this.right);
+
+        if (this.right) {
+            this.right.next = newNode;
+        } else {
+            this.left = newNode;
+        }
+
+        this.right = newNode;
+    }
+
+    removeLeft() {
+        if (!this.left) return null;
+
+        const { value } = this.left;
+        this.left = this.left.next;
+
+        if (this.left) this.left.previous = null;
+        else this.right = null;
+
+        return value;
+    }
+
+    removeRight() {
+        if (!this.right) return null;
+
+        const { value } = this.right;
+
+        this.right = this.right.previous;
+
+        if (this.right) this.right.next = null;
+        else this.left = null;
+
+        return value;
+    }
+
+    print() {
+        const result = [];
+        let current = this.left;
+        while (current) {
+            result.push(current.value);
+            current = current.next;
+        }
+        console.log(result.join(' -> '));
+    }
+}
+
+module.exports = LinkedList;
